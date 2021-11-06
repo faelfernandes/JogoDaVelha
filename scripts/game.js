2c369bf5816b0f3452bb96e0d2e5df48eba94716
habbioficial <contato@habbi.com.br>
2021-11-06 00:21:53 -0300

    Adicionando comentários

--- scripts/game.js
+++ scripts/game.js
@@ -3,12 +3,19 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
 let symbols = ['o', 'x'];
 
+// Pega a posição que foi passada na Interface.js 
 function handleMove(position) {
-  board[position] = symbols[playerTime];
 
-  if (playerTime == 0) {
-    playerTime = 1;
-  } else {
-    playerTime = 0;
+  // Garante que o board[position] é igual a vazio
+  if (board[position] == '') {
+    // Coloca o símbolo do jogador da vez
+    board[position] = symbols[playerTime];
+
+    // Muda a vez do jogador fazendo com que o símbolo seja alterado
+    if (playerTime == 0) {
+      playerTime = 1;
+    } else {
+      playerTime = 0;
+    }
   }
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,28 +1,39 @@
+// Criando Listener que será executado após o documento ser carregado
 document.addEventListener('DOMContentLoaded', () => {
+  // Pegar todos os squares do HTML
   let squares = document.querySelectorAll(".square");
 
+  // Varrer array de squares
   squares.forEach((squares) => {
+    // Pegar cada square dentro dele e colocar o eventL click e adicionar o handleClick para tratar o clique
     squares.addEventListener('click', handleClick);
   });
 });
 
 function handleClick(event) {
-  let squares = event.target;
-  let position = squares.id;
+  let squares = event.target; // Elemento que recebeu o evento
+  let position = squares.id; // Pega o id do square
 
-  console.log(squares);
-  console.log(position);
-
+  // Passa a posição para o handleMove do Game.js
   handleMove(position);
+  // Executa a função updateSquares
   updateSquares();
 }
 
+//Update de todos os squares
 function updateSquares() {
+  // Pega novamente todos os squares
   let squares = document.querySelectorAll(".square");
+
+  // Varre novamente cada um deles
   squares.forEach((square) => {
+    // Pega novamente a posição
     let position = square.id;
+    // Pega o símbolo que está no Array board que foi atualizado
     let symbol = board[position];
 
+    // Verifica se o símbolo não é vazio
+    // Se não for coloca X ou O
     if (symbol != '') {
       square.innerHTML = `<div class='${symbol}'></div>`;
     }
